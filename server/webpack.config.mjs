3c4e387722e491021193d5813c1e944633a983dd
jinxuchen2020 <jinxuchen2020@icloud.com>
2024-06-04 13:01:34 +0800

    remove vercel json

--- server/webpack.config.mjs
+++ server/webpack.config.mjs
@@ -37,7 +37,7 @@ const config = {
     new copyPlugin({
       patterns: [
         { from: 'public', to: __dirname +'/dist/public' },
-        { from: 'vercel.json', to: __dirname +'/dist/vercel.json' }
+        // { from: 'vercel.json', to: __dirname +'/dist/vercel.json' }
       ]
     }),
     new DefinePlugin({
